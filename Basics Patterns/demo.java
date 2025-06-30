4ea64545e0e5980fcb86c226ca8f558f559b3019
Sumeet1954 <thesumeetfacts@gmail.com>
2025-06-30 16:33:17 +0530

    Inverted star printing here

---
+++ Basics Patterns/demo.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class demo {
+
+    public static void main(String[] args) {
+        System.err.println("Enter The Number of Pattern");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j<=(n-i+1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
