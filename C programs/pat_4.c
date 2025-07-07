ac0fe622101c60a886e251d4feb9a7eb2640e491
Sumeet1954 <thesumeetfacts@gmail.com>
2025-07-07 23:23:13 +0530

    Add files via upload

---
+++ C programs/pat_4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main(){
+    int n ;
+    printf("Enter the number n : ");
+    scanf("%d",&n);
+    int size = 2 * (n - 1);
+    for(int i = 1; i <= size; i++){
+        for (int j = 0 ; j<=size; j++)
+        {
+                    
+    } 
+     printf("\n");  
+    }
+}
